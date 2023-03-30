997f1af5a8eb05d10e7f4f39218ebfe7b9220e94
“shivaniaspire” <shivani.padal@aspiresys.com>
2023-03-30 19:42:03 +0530

    Removed the secret pwd

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7-9snhyq=@&%^l0e6q#tk5_vtaow8t^b^bl5krjao67+xa_3n#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
